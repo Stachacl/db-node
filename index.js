7b57291652c85d8c6274a41cb257f81f9897d882
Stachacl <stacha.l@icloud.com>
2022-10-10 19:55:02 +1100

    fixed mistake

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 const express =require('express');
 const app = express();
 
-router.post('/register', (req,res) => {
-    res.send('Register');
-})
 
 app.listen(3000, () => console.log('Running!'));
