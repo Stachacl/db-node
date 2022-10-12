9ce02a3585c88aa1de93ba2f9790e7ddb6e15e4b
Stachacl <stacha.l@icloud.com>
2022-10-12 23:59:13 +1100

    connected to db

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-const express =require('express');
+const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+//Connenct to DB -change admin name first
+mongoose.connect('mongodb+srv://<admin name>:<LSPbCvMzUqpEOZrO>@cluster0.z3a5rdz.mongodb.net/?retryWrites=true&w=majority',
+{ useNewUrlParser: true },
+() => console.log('connected to db'))
 
 //Import Routes
 const authRoute =require('./routes/auth');
