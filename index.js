22dd71866e72753e95100f188f4445a3079cb8bd
Stachacl <stacha.l@icloud.com>
2022-10-13 16:06:26 +1100

    middleware added to index,js

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const  dotenv = require('dotenv');
+//Import Routes
+const authRoute =require('./routes/auth');
 
 dotenv.config();
-
 
 //Connenct to DB -change admin name first
 mongoose.connect( process.env.DB_CONNECT,
 { useNewUrlParser: true },
-() => console.log('connected to db'))
+() => console.log('connected to db'));
 
-//Import Routes
-const authRoute =require('./routes/auth');
-
+//Middleware
+app.use(express.json());
 //Route Middlewares
 app.use('api/user', authRoute); 
 
