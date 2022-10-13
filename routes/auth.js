874d8552bcba433a6131fb9befed752e03616a3c
Stachacl <stacha.l@icloud.com>
2022-10-13 16:08:24 +1100

    if we catch an error, whle subbmitting new user - return error 400

--- routes/auth.js
+++ routes/auth.js
@@ -2,13 +2,19 @@ const router = require('express').Router();
 const User = require('../model/User');
 
 
-router.post('/register', (req,res) => {
+router.post('/register', async (req,res) => {
     const user = new User({
         name: req.body.name,
         email: req.body.email,
         password: req.body.password
 
-    })
-})
+    });
+    try {
+
+    }catch(err) {
+        res.status(400).send(err);
+    }
+
+});
 // router.post('/login')
 module.exports =router;
